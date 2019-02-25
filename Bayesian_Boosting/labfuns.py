00e8e3ca7203134f953652aac5153a555a02a1da
Nga Nguyen <tthnga.nguyen@gmail.com>
2019-02-25 23:16:40 +0100

    modify labfuns to avoid warning

--- Bayesian_Boosting/labfuns.py
+++ Bayesian_Boosting/labfuns.py
@@ -253,6 +253,7 @@ def plotBoundary(classifier, dataset='iris', split=0.7):
             pass
         trClIdx = np.where(y[trIdx] == c)[0]
         teClIdx = np.where(y[teIdx] == c)[0]
+        color = color.reshape(1,len(color))
         plt.scatter(xTr[trClIdx,0],xTr[trClIdx,1],marker='o',c=color,s=40,alpha=0.5, label="Class "+str(c)+" Train")
         plt.scatter(xTe[teClIdx,0],xTe[teClIdx,1],marker='*',c=color,s=50,alpha=0.8, label="Class "+str(c)+" Test")
     plt.legend(bbox_to_anchor=(1., 1), loc=2, borderaxespad=0.)
